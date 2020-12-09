a6962c00f98ff6d1ea2fa62baf20510c48600c99
Oscar Costantini <oscar.costantini@studenti.unipd.it>
2020-12-09 12:36:26 +0100

    close #2

--- src/main/java/it/unipd/tos/business/TotalCalculator.java
+++ src/main/java/it/unipd/tos/business/TotalCalculator.java
@@ -11,20 +11,31 @@ import it.unipd.tos.business.exception.TakeAwayBillException;
 public class TotalCalculator implements TakeAwayBill {
     public double getOrderPrice(List < MenuItem > items, User user) throws TakeAwayBillException {
         double totale = 0.0;        
-
+        int numGel=0;
+        MenuItem GelMenoCost=null; 
 
         if (items == null) {
             throw new TakeAwayBillException("Lista vuota");
         }
 
         if (items.contains(null)) {
-            throw new TakeAwayBillException("Nessun elemento nella lista");
+            throw new TakeAwayBillException("Nessun Elemento nella lista");
         }
+        for(MenuItem i: items) {
+            totale +=i.getPrezzo();
+            if (i.getItemTipo() == MenuItem.type.Gelato) {
+                numGel++;
 
-        //costo
-        for (MenuItem i: items) { 
-            totale += i.getPrezzo();           
-        }
+                if ((GelMenoCost == null) || (GelMenoCost.getPrezzo() > i.getPrezzo())) {
+                    GelMenoCost = i;
+                }
+            }
+
+            } 
+            if (numGel > 5) {
+                totale -= GelMenoCost.getPrezzo() * 0.5;
+            }
+        
         return totale;     
     }
 }
